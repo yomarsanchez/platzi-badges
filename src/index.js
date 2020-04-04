6902d18af7108e44e2d460ded0b6d9d9de4477b1
yomarsanchez <yomarsan.98@gmail.com>
2020-04-04 11:41:10 -0500

    JSX html en .js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello Platzi Badges from React!</h1>
+// const elementJsx = <h1>Hello Platzi Badges from React!</h1>
+// const element = React.createElement('h1', {}, 'Hello Platzi Badges!!!')
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://platzi.com', target: '_black' },
+//   'Ir a platzi',
+// )
+const name = 'Yomar'
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`)
+// const elementJsx = <h1>Hola, soy {name}</h1>
+const elementJsx = (
+  <div>
+    <h1>Hola, soy {name}</h1>
+    <p>Soy inginiero backend</p>
+  </div>
+)
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, `Hola, soy ${name}`),
+  React.createElement('p', {}, 'Soy inginiero frontend'),
+)
 
 const container = document.getElementById('root')
-
 // ReactDOM.render(__que__, __dónde__);
 ReactDOM.render(element, container)
