02b09a3b31db02c62b4a8c4cb39a133bdccd1bb4
yomarsanchez <yomarsan.98@gmail.com>
2020-04-05 01:43:39 -0500

    Updates

---
+++ precache-manifest.bbfd3bf6d7bd420ad056e942bd374d60.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864181ae6793703c9231a34cc3a8f1d7",
+    "url": "/platzi-badges/index.html"
+  },
+  {
+    "revision": "9f9a284aa30cd4e4c645",
+    "url": "/platzi-badges/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "aa42e12bd4f6d9bfff17",
+    "url": "/platzi-badges/static/css/main.d6525415.chunk.css"
+  },
+  {
+    "revision": "9f9a284aa30cd4e4c645",
+    "url": "/platzi-badges/static/js/2.6485b7ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/platzi-badges/static/js/2.6485b7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa42e12bd4f6d9bfff17",
+    "url": "/platzi-badges/static/js/main.83dcc9d2.chunk.js"
+  },
+  {
+    "revision": "a4adcb377b2fb9bb0003",
+    "url": "/platzi-badges/static/js/runtime-main.65d2cca1.js"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/platzi-badges/static/media/twitter.c9f12008.svg"
+  }
+]);
